8b3921a25247ca4d3ca2f866217af15bf95bd098
Patrick <Patrick@Lenovo-E495>
2020-04-30 18:33:26 +0200

    Test für transitive Abhängigkeiten

--- Uebung2.2/src/sequenceTest.java
+++ Uebung2.2/src/sequenceTest.java
@@ -1,4 +1,3 @@
-import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,12 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class sequenceTest {
 
-    sequence test1, test2, test3;
+    sequence test1, test2, test3, test4;
     @BeforeEach
     void setup() {
         test1 = new sequence(new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }});
         test2 = new sequence(new String[][]{{ "A" , "B" }, { "B", "C" }, { "C", "D" }});
         test3 = new sequence(new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }});
+        test4 = new sequence(new String[][]{{ "A" , "B" }, { "B", "C" }});
     }
     
     @Test
@@ -47,10 +47,18 @@ public class sequenceTest {
     	assertFalse(test3.isWellSorted(new String[]{"G","A","B","C","D"}));
     }
     
+    @Test
+    @DisplayName("Transitive Abhängigkeiten werden korrekt behandelt")
+    void isWellSorted_transitiveAbh_entsprechendDerAbh() {
+    	assertFalse(test4.isWellSorted(new String[]{"C","A"}));
+    	assertFalse(test4.isWellSorted(new String[]{"B","C"}));
+    	assertTrue(test4.isWellSorted(new String[]{"A","C"}));
+    }
+    
     
     
     @Test
-    @DisplayName("Teste verschiedene Szenarien fÃ¼r isWellSorted()")
+    @DisplayName("Teste verschiedene Szenarien für isWellSorted()")
     void isWellSorted() {
         assertTrue(test1.isWellSorted(new String[]{"A","B","C","D"}));
         assertFalse(test2.isWellSorted(new String[]{"A","C","D"}));
